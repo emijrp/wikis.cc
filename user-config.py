b97ca63a7b052c8e33867020d0107a3c349fce2d
emijrp <emijrp@gmail.com>
2016-09-17 20:53:16 +0200

    .

---
+++ user-config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8  -*-
+
+mylang = 'wikiscc'
+family = 'wikiscc'
+
+usernames['wikiscc']['cadizpediadev'] = u'Emijrp'
+usernames['wikiscc']['wikandadev'] = u'Emijrp'
+#sysopnames['wikiscc']['cadizpediadev'] = u'Emijrp'
+
